72c6ccc4d8c57807f629cfa97fcf88e1d027916a
meshari alharbi <109522810+alharbisa24@users.noreply.github.com>
2025-07-24 23:30:59 +0300

    features

--- mltqa/dashboard/firebase_config.py
+++ mltqa/dashboard/firebase_config.py
@@ -4,7 +4,7 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-SERVICE_ACCOUNT_PATH = os.path.join(BASE_DIR, "firebase.json")
+SERVICE_ACCOUNT_PATH = os.path.join(BASE_DIR, "../firebase.json")
 
 if not firebase_admin._apps:
     cred = credentials.Certificate(SERVICE_ACCOUNT_PATH)
