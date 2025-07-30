c089008149ec627604f7045ab7adcc79d7404bf7
meshari alharbi <109522810+alharbisa24@users.noreply.github.com>
2025-07-30 12:43:08 +0300

    fix admin

--- mltqa/home/admin.py
+++ mltqa/home/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from dashboard import models as DashboardModels
+from home import models as HomeModels
+
+
 
 # Register your models here.
+admin.site.register(DashboardModels.Admin)
+admin.site.register(DashboardModels.Event)
+admin.site.register(HomeModels.Rating)
+admin.site.register(HomeModels.User)
+admin.site.register(HomeModels.Request)
