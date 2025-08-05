75551022d6076fda3b85a790acca749c1e9287b4
meshari alharbi <109522810+alharbisa24@users.noreply.github.com>
2025-08-05 16:13:54 +0300

    fix2

--- mltqa/build.sh
+++ mltqa/build.sh
@@ -11,5 +11,9 @@ python manage.py collectstatic --no-input
 # Apply any outstanding database migrations
 python manage.py migrate
 
-python manage.py createsuperuser --no-input || echo "Superuser may already exist"
-
+python manage.py shell << END
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', '1')
+END
