333bae65943d75e6fe503dab3cd4151ddf19163a
meshari alharbi <109522810+alharbisa24@users.noreply.github.com>
2025-09-04 18:19:44 +0300

    fixes

--- mltqa/mltqa/settings.py
+++ mltqa/mltqa/settings.py
@@ -78,15 +78,15 @@ WSGI_APPLICATION = 'mltqa.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        # Replace this value with your local database's connection string.
-        default=os.getenv('database_url'),
-        conn_max_age=600
-    )
-    #'default': {
-    #    'ENGINE': 'django.db.backends.sqlite3',
-    #    'NAME': BASE_DIR / 'db.sqlite3',
-    #}
+    #'default': dj_database_url.config(
+    #    # Replace this value with your local database's connection string.
+    #    default=os.getenv('database_url'),
+    #    conn_max_age=600
+    #)
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
